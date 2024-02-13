5466e91319d313fb0c250bfb0721e1c215008b05
TRICOT-Hugo <105285411+TRICOT-Hugo@users.noreply.github.com>
2024-02-13 19:05:34 +0100

    Fix bug with infinite testing loop (#52)
    * test: add positive test for compilation
    
    * test: add negative test for compilation
    
    * test: remove old negative test (see details) and add new test by checking files created
    
    The old test would loop infinitely after the push as it needs to test itself
    
    * refactor: rename test to show the repo does not exist instead of wrong commit
    
    * doc: add description of compilation testing in the README

--- ci/src/test/java/com/group21/ci/RepositoryTesterTest.java
+++ ci/src/test/java/com/group21/ci/RepositoryTesterTest.java
@@ -7,29 +7,11 @@ import org.junit.*;
 public class RepositoryTesterTest {
 
     /**
-     * Positive test for compilation. The RepositoryTester's runTest method
-     * clones and compiles the project on a known, working commit, and returns
-     * exit code 0 (all went well)
-     */
-    @Test
-    public void testCompilationOnWorkingCommit() {
-        RepositoryInfo repo = new RepositoryInfo("main",
-                                        "baeaaec85b5a3b480d842e1fa8a5253405f054b3",
-                                        "https://github.com/alexarne/DD2480-CI.git",
-                                           "alexarne",
-                                            "DD2480-CI");
-        RepositoryTester tester = new RepositoryTester(repo);
-        int exitCode = tester.runTests();
-        assertTrue(exitCode == 0);
-    }
-
-
-    /**
      * Negative test for compilation. The RepositoryTester's runTest method
-     * clones the project but cannot find the commit, whose id is wrong
+     * clones the project but cannot find the repo, as the url is wrong
      */
     @Test
-    public void testCompilationOnUnexistingCommit() {
+    public void testCompilationOnUnexistingRepo() {
         RepositoryInfo repo = new RepositoryInfo("main",
                                         "baeaaec85b5a3b480d842e1fa8a5253405f054b3",
                                         "https://github.com/alexarne/not-a-repo.git",
@@ -40,4 +22,6 @@ public class RepositoryTesterTest {
         assertTrue(exitCode != 0);
     }
 
+    // Note: a similar positive test was made but it ended up looping by checking itself. It was removed
+    // in favor of CompulingTest.testCreatedFilesFound()
 }
