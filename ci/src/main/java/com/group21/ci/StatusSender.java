c06e930b43d169096e6c7b0d4b6bb7cdb07d7e86
TRICOT-Hugo <105285411+TRICOT-Hugo@users.noreply.github.com>
2024-02-13 11:52:07 +0100

    Add documentation for the StatusSender class (#42)
    * doc: add documentation for the StatusSender class
    
    Co-authored-by: Juan Lavagnini <juanlavagnini@gmail.com>
    
    * fix: remove unused url
    
    ---------
    
    Co-authored-by: Juan Lavagnini <juanlavagnini@gmail.com>
    Co-authored-by: Alex Gunnarsson <75060613+alexarne@users.noreply.github.com>
    Co-authored-by: Alex Gunnarsson <alexgunnarsson@live.se>

--- ci/src/main/java/com/group21/ci/StatusSender.java
+++ ci/src/main/java/com/group21/ci/StatusSender.java
@@ -9,19 +9,29 @@ public class StatusSender {
     private String owner;
     private String repositoryName;
     private String SHA;
-    private String statusUrl;
     private HttpClient statusHttpClient;
     private String buildIdentifier;
     
+    /**
+     * StatusSender constructor
+     * @param repo    The RepositoryInfo representation of the repository
+     * @param id      Local identifier for the repo/build
+     */
     public StatusSender(RepositoryInfo repo, String id) {
         this.owner = repo.owner;
         this.repositoryName = repo.name;
         this.SHA = repo.commitId;
-        statusUrl = getStatusUrl();
         statusHttpClient = HttpClient.newHttpClient();
         this.buildIdentifier = id;
     }
 
+    /**
+     *  sendErrorStatus
+     *      - send a POST request to the repo to set commit status to error,
+     *      with a short description.
+     *      - the error status indicates the CI crashed and the job
+     *      could not be finished.
+     */
     public void sendErrorStatus() {
         System.out.println("Sent ERROR status");
         try {
@@ -35,6 +45,13 @@ public class StatusSender {
         }
     }
 
+    /**
+     *  sendFailureStatus
+     *      - send a POST request to the repo to set commit status to failure,
+     *      with a short description.
+     *      - the failure status indicates the CI did not crash but a problem occured
+     *      (such as a test failling).
+     */
     public void sendFailureStatus() {
         System.out.println("Sent FAILURE status");
         try {
@@ -48,6 +65,13 @@ public class StatusSender {
         }
     }
 
+    /**
+     *  sendPendingStatus
+     *      - send a POST request to the repo to set commit status to pending,
+     *      with a short description.
+     *      - the pending status indicates the CI is currently running and will update the
+     *      status when it finishes.
+     */
     public void sendPendingStatus() {
         System.out.println("Sent PENDING status");
         try {
@@ -61,6 +85,12 @@ public class StatusSender {
         }
     }
 
+    /**
+     *  sendSuccessStatus
+     *      - send a POST request to the repo to set commit status to success,
+     *      with a short description.
+     *      - the success status indicates the CI finished its work and returned normally
+     */
     public void sendSuccessStatus() {
         System.out.println("Sent SUCCESS status");
         try {
@@ -76,18 +106,32 @@ public class StatusSender {
 
 
     // Helper methods
+    /**
+     * getStatusUrl
+     *      - a helper method that creates the URL of the API to update the commit status 
+     * @return the URL of the API
+     */
     private String getStatusUrl() {
         return "https://api.github.com/repos/" + owner + "/" + repositoryName + "/statuses/" + SHA;
     }
 
+    /**
+     * requestBuilder
+     *      - creates, builds and returns a POST request to set the status 
+     * @param status    the status to set for the commit, must be "success",
+     *                  "pending", "error" or "failure"
+     * @param description   a short description associated with the commit status
+     * @return the built request
+     */
     public HttpRequest requestBuilder(String status, String description) {
 
-        // Add a target_url?
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(getStatusUrl()))
+            // Headers
             .header("Authorization", "Bearer " + Config.GITHUB_TOKEN)
             .header("Accept", "application/vnd.github+json")
             .header("X-GitHub-Api-Version", "2022-11-28")
+            // Body (inside a function declaring the HTTP method, here POST)
             .POST(HttpRequest.BodyPublishers.ofString("{\"state\":\"" + status + "\"" 
                 + "," + "\"description\":" + "\"" + description + "\""
                 + "," + "\"context\":\"project-continuous-integration-server\""
